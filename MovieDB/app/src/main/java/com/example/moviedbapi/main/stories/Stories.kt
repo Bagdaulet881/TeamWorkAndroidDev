46300d8b42885b2a7ce9e51dacda938d3e1c45d9
Bagdaulet881 <43570028+Bagdaulet881@users.noreply.github.com>
2020-04-29 19:41:58 +0600

    deleted to upload by pull request

--- MovieDB/app/src/main/java/com/example/moviedbapi/main/stories/Stories.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviedbapi.main.stories
-
-import com.example.moviedbapi.data.models.Story
-
-class Stories {
-    companion object {
-        var stories = ArrayList<Story>(5)
-    }
-}
